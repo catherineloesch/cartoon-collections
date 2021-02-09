48df80ec4e2c14df279462d01b0f8e75093f4acf
Catherine Loesch <catherine.loesch@protonmail.com>
2021-02-09 01:00:47 +0000

    learn test successful, learn submit failed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#Method 1
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#Method 2
+def summon_captain_planet(planeteer_calls)
+  new_calls = []
+  planeteer_calls.collect do |call|
+      new_call = call += "!"
+      new_calls << new_call.capitalize
+  end
+  new_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+#Method 3
+def long_planeteer_calls(array)
+  longer_than_4_characters = array.collect {|item| item.size > 4}
+  longer_than_4_characters.any?
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+#Method 4
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?("cheddar")
+    "cheddar"
+  elsif array.include?("gouda")
+    "gouda"
+  elsif array.include?("camenbert")
+    "camenbert"
+  else
+    nil
+  end
 end
+
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+cheese_types = ["cheddar", "gouda", "camembert"] 
